bab3b7501929d1ddac71f85f30a58115c1e6fb3f
Tunafan <124045808+Tunafan@users.noreply.github.com>
2023-12-07 17:33:28 +0100

    ny procedure til enkelt produkt

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -18,7 +18,9 @@ productsRouter.get("/", async (req, res) => {
 productsRouter.get("/:id", async (req, res) => {
     try {
         const { id } = req.params;
-        const [rows] = await connection.execute('SELECT * FROM products WHERE id = ?', [id]);
+
+        const sql = 'CALL LoadSingleArticle(?)'
+        const [rows] = await connection.execute(sql, [id]);
 
         if (rows.length > 0) {
             res.json(rows[0]); // Hvis produktet findes, returner det første element i arrayet
